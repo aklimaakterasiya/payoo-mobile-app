e97ec319b8a3b6449232676d86eaf1db70e511f5
aklimaakterasiya <aklimaakterasiya@gmail.com>
2026-03-12 19:19:24 +0600

    Added home page

--- script.js
+++ script.js
@@ -4,4 +4,24 @@ document.getElementById("loginButton").addEventListener("click", function(e){
     e.preventDefault()
     console.log("login button clicked")
     console.log(e)
+    const mobileNumber = 12345678910
+    const pinNumber = 1234
+
+
+    const mobileNumberValue = document.getElementById("mobile-number").value
+    const mobileNumberValueConverted = parseInt
+    (mobileNumberValue)
+
+    const pinNumberValue = document.getElementById("pin-number").value
+    const pinNumberValueConverted = parseInt
+    (pinNumberValue) 
+
+    console.log(mobileNumberValueConverted, pinNumberValueConverted)
+
+    if(mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pinNumber){
+        window.location.href="./home.html"
+    }
+    else{
+        alert("invalid credentials")
+    }
 })
